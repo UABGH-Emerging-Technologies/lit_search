c042232a195e17f4c18be40b9f152fefc39d2059
Ryan Melvin <rlmelvin@uab.edu>
2024-02-22 17:36:49 +0000

    known fixes for template

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 # Load packages from requirements.txt
 BASE_DIR = Path(__file__).parent
-with open(Path(BASE_DIR, "requirements.in")) as file:
+with open(Path(BASE_DIR, "requirements.txt")) as file:
     required_packages = [ln.strip() for ln in file.readlines()]
 
 docs_packages = ["mkdocs==1.3.1", "mkdocstrings==0.18.1"]
