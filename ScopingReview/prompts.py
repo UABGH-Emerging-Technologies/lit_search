cd60e093fad1727712480c5a30a0446c6aac3440
Ryan Godwin <rcg2@uab.edu>
2024-03-07 16:31:16 +0000

    WOrking to move things to data and scoping_review_app

--- ScopingReview/prompts.py
+++ ScopingReview/prompts.py
@@ -6,7 +6,7 @@ from langchain.prompts import (
 
 PUBMED_PROMPT = "Given the following research question, suggest a PubMed search string to find relevant articles:\n\n{}. Make the query sufficiently broad to be used to evaluate novelty of the project. Return only the pubmed search string, as your response will be used directly as an input to a function that takes in pubmed search strings."
 
-ITER_PUBMED_PROMPT = "Given the following list of keywords from articles selected by a human, suggest a PubMed search string to find more articles consistent with the relevant keywrods:\n\n{}. Make the query as succint as possible by combining similar keywords into the concepts they represent to return the most closely related articles given the set of keywords. Return only the pubmed search string, as your response will be used directly as an input to a function that takes in pubmed search strings."
+ITER_PUBMED_PROMPT = "Given the following list of keywords from articles selected by a human, suggest a PubMed search string to find more articles consistent with the relevant keywrods:\n\n{}. Make the query as succint as possible by combining similar keywords into the concepts they represent to return the most closely related articles given the set of keywords. Return only the pubmed search string, as your response will be used directly as an input to a function that takes in pubmed search string."
 
 FEW_RESULTS_PROMPT = "\n\n The following query returned no or few results. Please suggest a simpler one (i.e., with fewer query elements).\n\n"
 
