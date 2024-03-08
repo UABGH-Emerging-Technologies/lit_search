5c313086629dd2f379f3bb81e5a7377300f7fcf9
Ryan Godwin <rcg2@uab.edu>
2024-03-08 02:07:21 +0000

    Creating new compile.py file to handle categorize and summarize functions

---
+++ ScopingReview/compile.py
@@ -0,0 +1,9 @@
+
+
+class CategorizeManager:
+    def __init__(self, df, research_q):
+        self.df = df
+        self.research_q = research_q
+
+    def categorize_articles(self):
+        print("This is where the categorization specific logic will go")
