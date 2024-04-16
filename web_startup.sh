0239983acd9edecc23d04e784a4c3901416b176d
Ryan Melvin <rlmelvin@uab.edu>
2024-04-16 22:43:38 +0000

    correct startup script

--- web_startup.sh
+++ web_startup.sh
@@ -3,4 +3,4 @@
 #if weird dns errors, try
 #cp -f ./resolv.conf /etc/resolv.conf
 
-streamlit run Home.py --server.port=8501 --server.address=0.0.0.0
+streamlit run streamlit/ScopingReview_app.py --server.port=8501 --server.address=0.0.0.0
