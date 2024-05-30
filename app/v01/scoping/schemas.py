290dfcb70bbe1d8608c4331514b428dd45fd2601
Ryan Melvin <rlmelvin@uab.edu>
2024-05-30 12:59:53 -0500

    step4 byte encoding

--- app/v01/scoping/schemas.py
+++ app/v01/scoping/schemas.py
@@ -1,5 +1,5 @@
 
-from app.v01.schemas import XLSXinRequest
+from app.v01.schemas import SearchRequest, XLSXinRequest
 from typing import List
 from fastapi import Form
 # for consistency with our other apis, don't delete this import.
@@ -9,3 +9,6 @@ from ScopingReview.search import KeywordsData
 
 class CategoriesRequest(XLSXinRequest):
     user_defined_categories: str
+
+class SummariesRequest(SearchRequest, XLSXinRequest):
+    pass
