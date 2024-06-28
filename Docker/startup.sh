ac6741857d3190e29d8307de5857638ff79c4b8c
Ryan Godwin <rcg2@uab.edu>
2024-06-28 14:36:15 -0500

    fixed llm_utils imports and tested pubmed query generator

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -3,6 +3,7 @@
 #to test the package as you go
 python3 -m pip install pip setuptools wheel
 python3 -m pip install -e ".[dev]"
+python3 -m pip install -U ./llm_utils
 
 # Append PYTHONPATH to .bashrc to ensure it's set in all bash sessions
 echo 'export PYTHONPATH="/workspaces/scopingreview/llm_utils:${PYTHONPATH}"' >> ~/.bashrc
--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from pathlib import Path
-
 from setuptools import find_packages, setup
 
 # Load packages from requirements.txt
 BASE_DIR = Path(__file__).parent
-with open(Path(BASE_DIR, "requirements.txt")) as file:
-    required_packages = [ln.strip() for ln in file.readlines()]
+requirements_path = Path(BASE_DIR, "requirements.txt")
+required_packages = []
+dependency_links = []
+
+with open(requirements_path, encoding='utf8') as file:
+    for ln in file:
+        ln = ln.strip()
+        if ln.startswith('./'):  # handle local directory
+            dependency_links.append(ln)
+        else:
+            required_packages.append(ln)
 
 docs_packages = ["mkdocs", "mkdocstrings"]
 
 style_packages = ["black", "flake8", "isort"]
 
-dev_packages = ["pip-tools", "pandas", "pytest", "pytest-asyncio", "pytest-mock", "jupyter"]
+dev_packages = ["pip-tools", "pandas", "pytest", "pytest-asyncio", "pytest-mock"]
 
 # Define our package
 setup(
@@ -25,4 +33,5 @@ setup(
     packages=find_packages(),  # only look in directores with __init__.py
     install_requires=[required_packages],
     extras_require={"dev": docs_packages + style_packages + dev_packages, "docs": docs_packages},
+    dependency_links=dependency_links
 )
