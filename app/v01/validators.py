e5c9fd95d2354e16b46bff86015c3c7e27affeaf
Ryan Godwin <rcg2@uab.edu>
2024-07-17 13:27:50 -0500

    UPdating documentation, cleaning, and removing unused imports

--- app/v01/validators.py
+++ app/v01/validators.py
@@ -1,12 +1,12 @@
 from aiweb_common.file_operations.file_handling import create_base64_file_validator
 
-import app.fastapi_config as lit_api_config
+import app.fastapi_config as api_config
 
 validate_docx_bytes = create_base64_file_validator(
-    lit_api_config.DOCX_EXPECTED_TYPE
+    api_config.DOCX_EXPECTED_TYPE
 )
 
 
 validate_xlsx_bytes = create_base64_file_validator(
-    lit_api_config.XLSX_EXPECTED_TYPE
+    api_config.XLSX_EXPECTED_TYPE
 )
