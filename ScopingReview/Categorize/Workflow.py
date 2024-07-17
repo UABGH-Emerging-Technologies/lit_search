e8beec55daa6afce83050f155234201ffb978c10
Ryan Godwin <rcg2@uab.edu>
2024-07-17 11:51:48 -0500

    Have primary categorize functionality working

--- ScopingReview/Categorize/Workflow.py
+++ ScopingReview/Categorize/Workflow.py
@@ -2,6 +2,7 @@ from aiweb_common.WorkflowHandler import WorkflowHandler
 import pandas as pd
 import tempfile
 from ScopingReview.Categorize.Manager import FastAPICategorizeManager
+from ScopingReview_config import config, prompt_config
 from aiweb_common.generate.SingleResponse import SingleResponseHandler
 from typing import Tuple, Any
 
@@ -10,27 +11,42 @@ class CategorizeWorkflow(WorkflowHandler):
         super().__init__()
         self.df = df
         self.userdefined_categories = userdefined_categories
-        self.categorize_manager = FastAPICategorizeManager()
+        self.categorize_manager = FastAPICategorizeManager(self.df, self.userdefined_categories)
+        self.single_response = SingleResponseHandler(config.FAST_LLM_INTERFACE)
 
+    def _prep_df_for_categorization(self):
+        reduced_df = self.categorize_manager.get_relevant_rows().copy()
+        return reduced_df
+    
+    def _prep_categorylist(self):
+        input_list = self.userdefined_categories.split(",")
+        input_list = [value.strip() for value in input_list if value.strip()]
+        return input_list
+        
     def categorize_articles(self):
-        if self.df is not None:
-            reduced_df = self.get_relevant_rows(self.df).copy()
-            input_list = self.userdefined_categories.split(",")
-            input_list = [value.strip() for value in input_list if value.strip()]
 
-            for index, row in reduced_df.iterrows():
-                data = row[["abstract", "title"]]
-                assembled_prompt = self._assemble_prompt(thing_to_categorize=data)
-                response = self.single_response.generate_response(assembled_prompt)
-                self.single_response.update_total_cost()
-                assigned_categories = response.content.replace("'", "")
-                reduced_df.loc[index, "category"] = assigned_categories.lower()            
-            try:
-                full_text_df = self.categorize_manager.fetch_full_text(reduced_df['PMID'])
-                category_df = pd.merge(reduced_df, full_text_df, on='PMID', how='inner')
-            except Exception as e:
-                print(f"Failed while getting full texts: {str(e)}")
-            return category_df
+        reduced_df = self._prep_df_for_categorization()
+        input_list = self._prep_categorylist()
+
+        for index, row in reduced_df.iterrows():
+            data = row[["abstract", "title"]]
+            assembled_prompt = self.single_response.single_response_service.preparer.assemble_prompt(
+                system_prompt = prompt_config.CATEGORIZE_SYSTEM_TEMPLATE, 
+                user_prompt = prompt_config.CATEGORIZE_HUMAN_TEMPLATE, 
+                context=data,
+                categories = input_list
+                )
+            response, response_meta = self.single_response.generate_response(assembled_prompt)
+            self._update_total_cost(response_meta)
+            assigned_categories = response.content.replace("'", "")
+            reduced_df.loc[index, "category"] = assigned_categories.lower()            
+        try:
+            print("Fetching Full Texts")
+            full_text_df = self.categorize_manager.fetch_full_text(reduced_df['PMID'])
+            category_df = pd.merge(reduced_df, full_text_df, on='PMID', how='inner')
+        except Exception as e:
+            print(f"Failed while getting full texts: {str(e)}")
+        return category_df
 
     def get_tempfile_excel(self, category_df):
         category_df.drop_duplicates(subset="PMID", keep="first", inplace=True)
@@ -43,8 +59,7 @@ class CategorizeWorkflow(WorkflowHandler):
         return tmpfile.name
     
     def process(self):
-        category_df = self.categorize_articles()
-        if category_df is not None:
-            file_path = self.get_tempfile_excel(category_df)
-            return file_path
-        return None
+        if self.df is not None:
+            category_df = self.categorize_articles()
+
+            return category_df
