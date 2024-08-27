da3f9d1a56fa3e428f17c62b71d558c1e6aa920d
Ryan Melvin <rlmelvin@uab.edu>
2024-08-27 12:32:10 -0500

    handle long repetitive keywords lists

--- ScopingReview/prompts.py
+++ ScopingReview/prompts.py
@@ -10,7 +10,7 @@ from langchain.prompts import (
 
 PUBMED_PROMPT = """Given the following research question, suggest a PubMed search string to find relevant articles:\n\n{}. Make the query sufficiently broad to be used to evaluate novelty of the project. Return only the pubmed search string, as your response will be used directly as an input to a function that takes in pubmed search strings."""
 
-GENERATE_HUMAN_KEYWORD_PROMPT = """Given the scientific research question, a list of titles of scholarly publications, and a corresponding list of lists of keywords, each corresponding to a specific scholarly work from a PubMed search, your task is to analyze these inputs and determine three separate lists:
+GENERATE_HUMAN_KEYWORD_PROMPT = """Given the scientific research question, a list of titles of scholarly publications, and a corresponding list of keywords that researchers have identified from relevant articles in a PubMed search, your task is to analyze these inputs and determine three separate lists:
 
 Primary Keywords: These are the most relevant keywords that appear more frequently in the titles and keyword lists and are directly related to the research question. The presence of these keywords in an article title is a strong indicator that it could be useful for the research question.
 Secondary Keywords: These are the less frequent but still relevant keywords. They are related to the research question but might not have as direct of a correlation to the main topic as the primary keywords.
@@ -22,7 +22,7 @@ List of Publication Titles: \n\n{titles}
 
 List of keyword lists: \n\n{keywords_list}
 
-Please provide the primary, secondary, and exclusion keyword lists related to the given research question based on the provided keywords and titles lists from the PubMed search in JSON format."""
+Please provide the primary, secondary, and exclusion keyword lists related to the given research question based on the provided keywords and titles lists from the PubMed search in JSON format. Avoid repeats."""
 
 GENERATE_SYSTEM_KEYWORD_PROMPT = """ You are an expert clinical researcher. """
 
