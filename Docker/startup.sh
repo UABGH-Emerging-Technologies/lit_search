16c291d7063ff4ea080d1146895125848f574542
prashant <prashantsharma@uabmc.edu>
2025-07-28 09:58:16 -0500

    LLM_Utils removed

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -3,8 +3,3 @@
 #to test the package as you go
 python3 -m pip install pip setuptools wheel
 python3 -m pip install -e ".[dev]"
-python3 -m pip install -U ./llm_utils
-
-# Append PYTHONPATH to .bashrc to ensure it's set in all bash sessions
-echo 'export PYTHONPATH="/workspaces/scopingreview/llm_utils:${PYTHONPATH}"' >> ~/.bashrc
-echo 'export PYTHONPATH="/workspaces/scopingreview/llm_utils:${PYTHONPATH}"' >> ~/.zshrc
