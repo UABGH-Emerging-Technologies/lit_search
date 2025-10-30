941d9446c3f12abc04b6d633f7f18c6e5bb15824
Ryan Melvin <rmelvin@uabmc.edu>
2025-10-30 11:38:11 -0500

    remove unneeded secrets

--- ScopingReview_config/app_config.py
+++ ScopingReview_config/app_config.py
@@ -2,16 +2,7 @@ from aiweb_common.WorkflowHandler import manage_sensitive
 
 NAME = "lit"
 
-# Database
-DB_SERVER = manage_sensitive("db_server")
-DB_NAME = manage_sensitive("db_name")
-DB_USER = manage_sensitive("db_user")
-DB_PASSWORD = manage_sensitive("db_password")
-
 # LLM specific
-OPENAI_API_KEY = manage_sensitive("openai_api_key")
-GPT4_KEY = manage_sensitive("gpt4_api_key")
-
 # libkey
 LIBKEY_API_KEY = manage_sensitive("libkey_api_key")
 
